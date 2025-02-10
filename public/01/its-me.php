487d78f0a9159b0b0ded31afca5b963c0cf3554e
Benji <106337182+Fisjes@users.noreply.github.com>
2025-02-10 12:51:17 +0100

    5 echo's toegevoegd

---
+++ public/01/its-me.php
@@ -0,0 +1,4 @@
+<?php
+
+echo "Mijn favoriete film: Interstellar. Favoriete game: Sound Voltex. Klas: SD1C";
+?>
---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "<h1>Hello, world!</h1>";
+echo "<p>Schrijven is niet perse leuk want het is saai maar als ik wil yappen kan dat dus gewoon hier.</p>";
+echo "<img src='https://i.ytimg.com/vi/rQE5MptQO-g/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLCLOyxcpWzyV0xXwpbwH27x3LOpnQ'>";
+echo "<h2>XD</h2>";
+echo "<img src='images/Carlos.png'>";
+?>
