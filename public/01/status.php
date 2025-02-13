a90f24c2bde7579968e3bd589fc1e6a6ad340bec
Benji <106337182+Fisjes@users.noreply.github.com>
2025-02-13 11:49:16 +0100

    variables en comments toegevoegd

---
+++ public/01/status.php
@@ -0,0 +1,50 @@
+<?php
+
+//status
+$status = "offline";
+echo $status;
+
+echo "<br>"; 
+
+$status = "available";
+echo $status;
+
+echo "<br>"; 
+
+$status = "busy";
+echo $status;
+
+echo "<br>";
+
+$status = "Do not disturb";
+echo $status;
+
+echo "<br>";
+echo "<br>";
+
+//prijs
+$prijs = "Prijs";
+echo $prijs;
+
+echo "<br>";
+
+$prijs = "2.40";
+echo $prijs;
+
+echo "<br>";
+
+$prijs = "3.50";
+echo $prijs;
+
+echo "<br>";
+
+$prijs = "4.40";
+echo $prijs;
+
+echo "<br>";
+
+$prijs = "5.50";
+echo $prijs;
+
+echo "<br>";
+?>
