61767076420e714aa25e0310656d629c9fb41c62
Benji <106337182+Fisjes@users.noreply.github.com>
2025-02-13 12:09:17 +0100

    variables gefixt

---
+++ public/01/variabelennaam.php
@@ -0,0 +1,12 @@
+<?php
+$auto2 = "mercedes";
+$_naam = "benji";
+$auto = "beetle"; //kan geen speciale leesteken bevatten
+$autoonderdelen = "motor"; //kan geen slash bevatten
+$huistuin = "boompje"; //kan geen speciale leesteken bevatten
+$naw = "jah";
+$tweeentwintig = "cents"; //kan geen cijfers bevatten
+$$auto = "luek";
+$kleurVanAuto = "geel";
+$softwaredev = "beter dan game dev"; //kan geen speciale leesteken bevatten
+?>
