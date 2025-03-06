e57ce31aa10f697af054b8e4ce7530b1a3f35796
Benji <106337182+Fisjes@users.noreply.github.com>
2025-03-06 11:53:36 +0100

    Arrays index gemaakt
    Yep

---
+++ public/04/arrays_index.php
@@ -0,0 +1,12 @@
+<?php
+
+$dagen = ["maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag"];
+
+echo $dagen[4];
+echo "<br>";
+echo $dagen[2];
+echo "<br>";
+echo $dagen[6];
+echo "<br>";
+echo $dagen[1];
+?>
