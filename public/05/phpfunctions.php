eb07300521b0d1bb49a7d158c6e6e95666b07b4e
Benji <106337182+Fisjes@users.noreply.github.com>
2025-03-17 11:59:37 +0100

    Verder gemaakt

--- public/05/phpfunctions.php
+++ public/05/phpfunctions.php
@@ -14,4 +14,28 @@ $anderLangWoord = "Supercalifragilisticexpialidocious";
 $anderWoordLengte = strlen($anderLangWoord);
 print($anderWoordLengte);
 
+echo "<br>";
+
+$tweeMacht2 = pow(2,10);
+
+print $tweeMacht2;
+
+//vraag 1: print $variable;
+
+//https://www.php.net/manual/en/function.strtoupper.php
+
+echo "<br>";
+
+$naam = "Wario";
+$naamGroot = strtoupper($naam);
+print $naamGroot;
+
+echo "<br>";
+
+$naam2 = "WALUIGI";
+$naamKlein = strtolower($naam2);
+print $naamKlein;
+
+
+
 ?>
